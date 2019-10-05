602b8b005d7b4e1b6c0658493907e968c42fe357
Kyo Yoshida <nirufuyu@gmail.com>
2019-10-05 18:53:46 +0900

    pypiの準備

---
+++ practical_stacking/__init__.py
@@ -0,0 +1,10 @@
+from stack import *
+
+__copyright__ = 'Copyright (C) 2018 takuya-andou'
+__version__ = '1.0.0'
+__license__ = 'MIT License'
+__author__ = 'nirufuyu'
+__author_email__ = 'nirufuyu@gmail.com'
+__url__ = 'https://github.com/takuya-andou/stacking'
+
+__all__ = ['stack']
